40327159a50545241c5865a24f54d0e87a2e02ae
Piotr Ksiazek <piotrksiazek565@gmail.com>
2023-02-21 23:39:40 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -1,39 +1,57 @@
 console.log("Cześć!");
 {
     const tasks = [];
+    
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+    
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li>
+            <button class="js-remove">usuń</button>
             ${task.content}
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-    const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
+
+        const removeButtons = document.querySelectorAll(".js-remove")
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
         });
-        
-        render();
     };
+    
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        
+
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-        
+
         if (newTaskContent === "") {
             return;
         }
 
         addNewTask(newTaskContent);
 
-        
+
     }
     const init = () => {
         render();
